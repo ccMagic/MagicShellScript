2dfeb3a0a13bbee9d97e159715f4e109e7efbdce
chucan <mchucan@hotmail.com>
2019-06-21 16:35:25 +0800

    关系运算符

--- operator.sh
+++ operator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo --------------------start--------------------
 echo ------------------ 运算符--------------------
-
+echo ------------------算数运算符-----------------
 val=`expr 2 + 2`
 echo "两数之和："$val
 
@@ -41,3 +42,67 @@ if [ $a!=$b ]
 then 
 	echo "a不等于b"
 fi
+
+echo
+echo
+echo --------------------关系运算符-------------------
+echo 关系运算符只支持数字，不支持字符串，除非字符串的值是数字
+a=10
+b=20
+echo a=$a
+echo b=$b
+echo 检测两个数是否相等:-eq
+if [ $a -eq $b ]
+then
+	echo "\$a -eq \$b : a 等于 b"
+else
+	echo "\$a -eq \$b : a 不等于 b"
+fi
+echo
+echo 检测两个数据是否不相等:-ne
+if [ $a -ne $b  ]
+then
+	echo "\$a -ne \$b: a 不等于 b"
+else
+	echo "\$a -ne \$b: a 等于 b"
+fi
+echo
+echo 检测左边的数是否大于右边的数，如果是，返回true：-gt
+if [ $a -gt $b ]
+then
+	echo "\$a -gt \$b:" a 大于 b
+else
+	echo "\$a -gt \$b:" a 不大于 b
+fi
+echo
+echo 检测左边的数是否小于右边的，如果是，则返回true：-lt
+if [ $a -lt $b ]
+then
+	echo "\$a -lt \$b:" a 小于 b
+else
+        echo "\$a -lt \$b:" a 不小于 b	
+fi
+
+echo
+echo 检测左边的数是否大于等于右边的，如果是，则返回true
+if [ $a -ge $b ]
+then
+	echo "\$a -ge \$b:" a 大于等于 b
+else
+	echo "\$a -ge \$b:" a 不大于等于（小于） b
+fi
+
+echo
+echo 检测左边的数是否小于等于右边的，如果是，则返回true
+if [ $a -le $b ]
+then 
+	echo "\$a -le \$b:" a 小于等于 b
+else
+	echo "\$a -le \$b:" a 不小于等于（大于） b
+fi
+
+echo
+echo
+echo ------------------end---------------------------
+
+
